a8259339362f23a39fc0c2e8938f327c1e128ea0
kodle <theobourdier1@gmail.com>
2020-08-02 19:20:46 +0200

    Saved / Don't work

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ path = input("Path:")
 
 im = Image.open(path)
 exif = im.getexif()
-creation_time = exif.get(36867)
+creation_time = exif.get(36868)
 
-print(creation_time)
+def changeWord(creation_time):
+    for letter in creation_time:
+        if letter != ":":
+            word = word.replace(letter,"-")
+        return word
+
+try:
+    os.mkdir(creation_time)
+    print(creation_time ,  " created ")
+except FileExistsError:
+    print(creation_time , " already exists")
