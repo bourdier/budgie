b815189a189cd49d85d003a66deee08f1303882f
kodle <theobourdier1@gmail.com>
2020-08-02 21:19:16 +0200

    Copy photo to the new folder

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-import sys
-import os
+import sys, os, shutil
 from PIL import Image
 
-path = input("Path:")
+imgPath = input("Image name:")
+imgFolderPath = input("Image folder path:")
+oldPath = imgFolderPath + '\\' + imgPath
 
-im = Image.open(path)
-exif = im.getexif()
+img = Image.open(oldPath)
+exif = img.getexif()
 creation_time = exif.get(36868)
 
 remove_characters = [":"]
 for character in remove_characters:
     creation_time = creation_time.replace(character, "-")
 
+newPath = imgFolderPath + creation_time + '\\'
+images = [oldPath]
+
 try:
     os.mkdir(creation_time)
-    print(creation_time ,  "created ")
+    print(creation_time,  "created")
+    for f in images:
+        shutil.copy(f, creation_time)
+    print(newPath, "copied")
 except FileExistsError:
-    print(creation_time , "already exists")
+    print(creation_time, "already exists")
+
+# a.jpg
+# C:\\Users\\Théo Jeux\\Desktop\\
