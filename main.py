fdb0a4eb82056192f51dfc0bfe68346d4d77b73f
kodle <theobourdier1@gmail.com>
2020-08-02 20:24:51 +0200

    Folder creation from photo EXIF

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ im = Image.open(path)
 exif = im.getexif()
 creation_time = exif.get(36868)
 
-def changeWord(creation_time):
-    for letter in creation_time:
-        if letter != ":":
-            word = word.replace(letter,"-")
-        return word
+remove_characters = [":"]
+for character in remove_characters:
+    creation_time = creation_time.replace(character, "-")
 
 try:
     os.mkdir(creation_time)
-    print(creation_time ,  " created ")
+    print(creation_time ,  "created ")
 except FileExistsError:
-    print(creation_time , " already exists")
+    print(creation_time , "already exists")
